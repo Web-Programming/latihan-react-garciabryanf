08cd15881ca3d08a65249b5d9b719daae609bcc9
garciabryanf <garciabryanfarrel_2327250026@mhs.mdp.ac.id>
2024-09-30 20:32:33 +0700

    Error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
-import Hello from './Hello';
-import Product from './Product';
-import Registrasi from './Registrasi';
+import { NavLink, Route } from 'react-router-dom';
+import routes from './Route';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Hello />
-        <Hello nama="Bryan" pesan="Selamat semoga beruntung"/>
-          <Product />
-            <Registrasi />
-      </header>
-    </div>
+   <React.Suspense fallback={<div>Loading ...</div>}>
+    <div className='App'>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+         <div class="container-fluid">
+        </div>
+        <NavLink to="/" className="navbar-brand">
+          navbar
+        </NavLink>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+        <li class="nav-item">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/Product" className="nav-link">Product</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/Register" className="nav-link">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+        </div>
+      </nav>
+      </div>
+    
+
+
+
+
+<div className='main'>
+  <Route>
+    {
+      routes.map((route, i) => {
+    const {path, Component } =  route;
+    return <Route key={i} path={path} element={<Component />} />;
+      })}
+  </Route>
+ </div>
+
+   </React.Suspense>
   );
 }
 
